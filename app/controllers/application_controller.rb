7162e1e643f3c27e5c3066a2cf45f4776dde97f6
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-03-04 17:47:32 +0000

    added pundit policies folder and policies for application, user, and photo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
+  include Pundit
   before_action :authenticate_user!
-
   before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  after_action :verify_authorized, except: :feed
+
+
+  private
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    
+    redirect_back fallback_location: root_url
+  end
 
   protected
 
